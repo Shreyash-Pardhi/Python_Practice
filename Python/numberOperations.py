62881f88e342d078f4908b134ac7b92f03a73582
unknown <shreyashpardhi1@gmail.com>
2024-05-09 16:13:01 +0530

    python practice 9/5/2024

---
+++ Python/numberOperations.py
@@ -0,0 +1,11 @@
+import math as m
+print(2**3) # Square
+
+print(5//3) #flore division
+
+print(5/3) #normal division
+
+print(oct(20) ,bin(20), hex(20)) #octal, binalry, hex
+
+print(m.trunc(-2.9)) # rounds of the number closest to zero
+print(m.floor(-2.9)) # rounds off the value closest to negative infinity
