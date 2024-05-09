9719ff5d61217948bc823fcf0fe902517e0b0e0a
unknown <shreyashpardhi1@gmail.com>
2024-05-09 15:33:10 +0530

    python practice programs 9/5/2024

--- Python/addAndSubMatrix.py
+++ Python/addAndSubMatrix.py
@@ -1,4 +1,3 @@
 import numpy as np
-a =np.array([[2,3],[4,5]])
-b =np.array([[1,1],[1,1]])
-print(np.sum(a,b))
+a = [[2,3],[4,5]]
+b = [[1,1],[1,1]]
---
+++ Python/sample.py
@@ -0,0 +1,8 @@
+try:
+    a=7
+    b=2.0
+    print(a//b)
+except Exception as e:
+    print(e)
+finally:
+    print('Finally end of program')
