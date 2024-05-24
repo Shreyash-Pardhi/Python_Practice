f792af987c9b72cda3a77dbc1dc0902aa53c4a8e
unknown <shreyashpardhi1@gmail.com>
2024-05-24 18:43:47 +0530

    Final NGG game with GUI

--- Python/Python Projects/Number Guessing Game/NGG.py
+++ Python/Python Projects/Number Guessing Game/NGG.py
@@ -1,26 +1,24 @@
 import random
-import NGG_GUI
 
 if __name__ == '__main__':
     print("Number Guessing Game\nEnter the range...")
-    From = int(NGG_GUI.From.get())
-    To = int(NGG_GUI.To.get())
+    From = int(input("from :"))
+    To = int(input("to :"))
     num = random.randint(From, To)
     totalAttempts =(From + To).bit_length()
-    #c=0
-    def Operations():
-        if (From and To)!=None:
-            while(totalAttempts>0):
-                print(f"\nYou have {totalAttempts} attempts")
-                guess = int(input("Guess the number :"))
-                if(guess>num):
-                    print("Guess is high")
-                    totalAttempts-=1
-                elif(guess<num):
-                    print("Guess is low")
-                    totalAttempts-=1
-                else:
-                    print("Congratulations, You guessed it!!")
-                    break
-        if(totalAttempts==0):
-            print("\nNice Try, Better luck next time...")
+    
+    if (From and To)!=None:
+        while(totalAttempts>0):
+            print(f"\nYou have {totalAttempts} attempts")
+            guess = int(input("Guess the number :"))
+            if(guess>num):
+                print("Guess is high")
+                totalAttempts-=1
+            elif(guess<num):
+                print("Guess is low")
+                totalAttempts-=1
+            else:
+                print("Congratulations, You guessed it!!")
+                break
+    if(totalAttempts==0):
+        print("\nNice Try, Better luck next time...")
--- Python/Python Projects/Number Guessing Game/NGG_GUI.py
+++ Python/Python Projects/Number Guessing Game/NGG_GUI.py
@@ -1,31 +1,88 @@
 from tkinter import *
-import NGG
+import random
+num =0
+totalAttempts =0
 
+def ffrroomm(*args):
+    global totalAttempts
+    global num
+    v1 = int(e1.get())
+    v2 = int(e2.get())
+    sm = (v1 + v2).bit_length()-1
+    totalAttempts = sm
+    num = random.randint(v1, v2)
+    lbl.set(sm)
+    
+def Operations():
+    try: 
+        global num
+        global totalAttempts
+        lbl.set(totalAttempts-1)
+        guess = int(Guess.get())
+        if(num == 0):
+            lbl.set(0)
+            result["text"]="Please enter\nthe range first"
+            return
+        if(guess>num):
+            result["text"]="Guessed number is\ntoo high"
+            totalAttempts-=1
+        elif(guess<num):
+            result["text"]= "Guessed number is\ntoo low"
+            totalAttempts-=1
+        else:
+            result["text"]= "Congratulations\nYou guessed it!!"
+            e1.set('')
+            e2.set('')
+            num = None
+            return
+        if(totalAttempts==0):
+            result["text"]= "Nice Try\nBetter luck next time..."
+            e1.set('')
+            e2.set('')
+            num = None
+            return
+    except Exception as e:
+        result["text"] = "Please\nproperly enter the range\nand number to guess"
+        lbl.set(0)
+    
 root = Tk()
 root.geometry("400x300")
+root.maxsize(400,300)
+root.minsize(400,300)
 root.title("Number Guessing Game")
+root.configure(bg="#40A578")
 
-Label(root,text="Number Guessing Game",font=("bold",20)).place(x=50,y=15)  
-Label(root, text="Enter the range to choose from..",font=(15)).place(x=15,y=70) 
 
-Label(root,text="From: ",font=(10)).place(x=15,y=100) 
-From = Entry(root)
+Label(root,text="Number Guessing Game",font=("bold",20),bg="#40A578").place(x=50,y=15)  
+Label(root, text="Enter the range to choose from..",font=(15),bg="#40A578").place(x=15,y=70) 
+
+e1 = StringVar()
+e2 = StringVar()
+e1.trace_add("write", ffrroomm)
+e2.trace_add("write", ffrroomm)
+
+Label(root,text="From: ",font=(10),bg="#40A578").place(x=15,y=100) 
+From = Entry(root,textvariable=e1,bg="#9DDE8B")
 From.place(x=70,y=103)
 
-Label(root,text="To: ",font=(10)).place(x=15,y=125)
-To = Entry(root)
+Label(root,text="To: ",font=(10),bg="#40A578").place(x=15,y=125)
+To = Entry(root, textvariable=e2,bg="#9DDE8B")
 To.place(x=70,y=128)
 
-Label(root,text="Attempts: ",font=(10)).place(x=15,y=155)
-Label(root,text="0",font=(10)).place(x=85,y=156)
+lbl = StringVar()
+lbl.set('0')
+Label(root,text="Attempts: ",font=(10),bg="#40A578").place(x=15,y=155)
+attrmpt = Label(root,font=(10),textvariable=lbl,bg="#40A578")
+attrmpt.place(x=85,y=156)
 
-Label(root,text="Guess no.: ",font=(10)).place(x=15,y=181)
-Guss = Entry(root,width=15)
-Guss.place(x=100,y=184)
+Label(root,text="Guess no.: ",font=(10),bg="#40A578").place(x=15,y=181)
+Guess = Entry(root,width=15,bg="#9DDE8B")
+Guess.place(x=100,y=184)
 
-Label(root,text=".....Result.....",font=(10),state="disabled",width=19,height=5,bg="#dbd7d2").place(x=210,y=100) 
+result = Label(root,text="",font=(10),width=19,height=5,bg="#E6FF94")
+result.place(x=210,y=100)
 
-button = Button(text="Guess",font=(15),width=30,bg="#555555",command=NGG.Operations).place(x=55,y=225)
+button = Button(text="Guess",font=("bold",15),width=30,bg="#006769",command=Operations).place(x=30,y=225)
 
 
 root.mainloop()
