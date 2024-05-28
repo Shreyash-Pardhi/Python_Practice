65f18be57be9d5804453726b9584eee697bd9466
unknown <shreyashpardhi1@gmail.com>
2024-05-28 18:38:02 +0530

    Python Practice 28/05/2024

--- Python/fileHandling.py
+++ Python/fileHandling.py
@@ -3,7 +3,8 @@ try:
     # d=f.read()
     # print("'",d,"'")
     f = open('file.txt')
-    d=f.write('writing content in a file')
+    d=len(f.readline())
+    print(d)
     f.close()
 except Exception as e:
     print("Open file in writeable format to write" if(str(e)=="not writable") else e)
