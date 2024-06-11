23ff1f52323b910ad9038a8d76afcb111d8664b9
unknown <shreyashpardhi1@gmail.com>
2024-06-11 18:52:14 +0530

    updates 11/06/2024

--- Python/Assessment-2 (GOOGLE VISION API)/Additional-Task/main.py
+++ Python/Assessment-2 (GOOGLE VISION API)/Additional-Task/main.py
@@ -1,8 +1,9 @@
 import gradio as gr
 from google.cloud import vision
 import os
-
+import cloudinary
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "D:\\Work and Assignments\\Python\\Assessment-2 (GOOGLE VISION API)\\Additional-Task\\storage_key.json"
+
 
 def Feature_Extraction(img_path):
     l=lbl=txt=''
@@ -16,6 +17,8 @@ def Feature_Extraction(img_path):
             content = image_file.read()
         img = vision.Image(content=content)
         
+        # objects = client.object_localization(image=img).localized_object_annotations
+        # obj = [object_.name]
         #Brand
         res_logo = client.logo_detection(img)
         logos = res_logo.logo_annotations
@@ -38,18 +41,23 @@ def Feature_Extraction(img_path):
         gr.Warning(f"Something happened!!!, \nPlease refresh and start again...")
     
     return (l,lbl,txt)
-    
-        
-brand = gr.Text(label="Brand")
+
+# def save_to_csv()
+
+
+brand = gr.Text(label="Brand / Logo")
 labels = gr.Text(label="Labels")
 text = gr.Text(label="Text")
 
-demo = gr.Interface(fn= Feature_Extraction,
-                    inputs=gr.Image(label='Product Image',type='filepath'),
-                    outputs= [brand, labels, text],
-                    title="Image Feature Extraction",
-                    description="Upload an Image, and see extracted features from image using Google Vision API.<p>Features Like; Logo Detection, Labels Detection, Text Detection.</p>",
-                    allow_flagging='never',
-                    css="footer {visibility: hidden}")
+with gr.Blocks(title="Image-Feature Extraction",
+               css="footer {visibility: hidden}"
+            ) as demo:
+        gr.Interface(fn= Feature_Extraction,
+                        inputs=gr.Image(label='Upload Product Image',type='filepath'),
+                        outputs= [brand, labels, text],
+                        title="<p style='color:orange; font-size:35px'>Image Feature Extraction</p><br>",
+                        description="<p style='font-size:15px'>Upload an Image, and see extracted features from image using Google Vision API.<br>Features Like; Logo Detection, Labels Detection, Text Detection.</p>",
+                        allow_flagging='never',
+                    )
 
 demo.launch(debug= True)
