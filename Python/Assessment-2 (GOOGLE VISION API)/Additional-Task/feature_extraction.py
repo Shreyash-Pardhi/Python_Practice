abf5b1c920685ab69f5d1ad77fb2bc954ce81e7e
Shreyash Pardhi <shreyashpardhi1@gmail.com>
2024-06-16 11:06:43 +0530

    updates 15/06/2024

---
+++ Python/Assessment-2 (GOOGLE VISION API)/Additional-Task/feature_extraction.py
@@ -0,0 +1,67 @@
+import gradio as gr
+from google.cloud import vision
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "D:\\Work and Assignments\\Python\\Assessment-2 (GOOGLE VISION API)\\Additional-Task\\storage_key.json"
+
+
+def Feature_Extraction(img_path):
+    l=lbl=txt=ob=''
+    try:
+        if(img_path == None):
+            gr.Warning("Please upload a Image")
+            return (l,lbl,txt)
+     
+        client = vision.ImageAnnotatorClient()
+        with open(img_path, "rb") as image_file:
+            content = image_file.read()
+        img = vision.Image(content=content)
+        
+        # objects = client.object_localization(image=img).localized_object_annotations
+        # obj = [object_.name]
+        #Brand
+        res_logo = client.logo_detection(img)
+        logos = res_logo.logo_annotations
+        logo = [log.description for log in logos]
+        l = f"Brand not detected!!" if len(logo)==0 else f"{logo[0]}"
+        
+        #For retriving Labels
+        res_label = client.label_detection(img)
+        labels = res_label.label_annotations
+        label = [lab.description for lab in labels]
+        lbl =f"Labels not detected!!" if len(label)==0 else f"{",  ".join(label)}"
+        
+        #For retriving Text
+        res_text = client.document_text_detection(img)
+        texts = res_text.text_annotations
+        text = [t.description for t in texts]
+        txt = f"Text not detected!!" if len(text)==0 else f"{text[0]}"
+
+        objects = client.object_localization(image=img).localized_object_annotations
+        object = [o.name for o in objects]
+        ob = f"Object not detected!!" if len(object)==0 else f"{object}"
+        
+    except Exception as e:
+        gr.Warning(f"Something happened!!!, \nPlease refresh and start again...:{e}")
+    
+    return (l,lbl,txt,ob)
+
+# def save_to_csv()
+
+
+brand = gr.Text(label="Brand / Logo")
+labels = gr.Text(label="Labels")
+text = gr.Text(label="Text")
+obj = gr.Text(label="Object")
+
+with gr.Blocks(title="Image-Feature Extraction",
+               css="footer {visibility: hidden}"
+            ) as features:
+        gr.Interface(fn= Feature_Extraction,
+                        inputs=gr.Image(label='Upload Product Image',type='filepath'),
+                        outputs= [brand, labels, text, obj],
+                        title="<p style='color:orange; font-size:35px'>Image Feature Extraction</p>",
+                        description="<p style='font-size:15px'>Upload an Image, and see extracted features from image using Google Vision API.<br>Features Like; Logo Detection, Labels Detection, Text Detection and Objects Detection.</p>",
+                        allow_flagging='never',
+                    )
+
+features.launch(debug=True)
---
+++ Python/Assessment-2 (GOOGLE VISION API)/Additional-Task/product_search_obj.py
@@ -0,0 +1,79 @@
+import gradio as gr
+from google.cloud import vision
+import os
+import pandas as pd
+import ast
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "D:\\Work and Assignments\\Python\\Assessment-2 (GOOGLE VISION API)\\Additional-Task\\storage_key.json"
+
+#Reading Data From Google Cloud Bucket CSV
+DB = pd.read_csv("gs://bucket-shreyash/Product_Data/Products_v2.csv")
+
+#features Present in database csv file
+DB_features = DB['objects_extracted']
+
+#Extracting features from input image
+def Input_IMG_features(img_path):
+    txt=''
+    try:
+        if(img_path == None):
+            gr.Warning("Please upload a Image")
+            return txt
+
+        #reading input image
+        client = vision.ImageAnnotatorClient()
+        with open(img_path, "rb") as image_file:
+            content = image_file.read()
+        img = vision.Image(content=content)
+        
+        #For retriving Labels
+        res_label = client.label_detection(img)
+        labels = res_label.label_annotations
+        label = [lab.description for lab in labels]
+        
+        #for objects
+        objects = client.object_localization(image=img).localized_object_annotations
+        obj = [ob.name for ob in objects]
+        txt = txt + f"{label}" if len(obj)==0 else txt + f"{obj}"
+        
+    except Exception as e:
+        gr.Warning(f"Something happened!!!, \nPlease refresh and start again...")
+    
+    return check_relevent_products(txt)
+
+def check_relevent_products(inp_features):
+    try: 
+        name = []
+        link = []
+        inp_features = set(ast.literal_eval(inp_features))
+        
+        for i in range(len(DB_features)):
+            DB_obj = set(ast.literal_eval(DB_features[i]))
+            if len(inp_features.intersection(DB_obj)) !=0:
+                name.append(DB.iloc[i]['product_name'])
+                link.append(DB.iloc[i]['product_url'])
+        
+        if(len(link)==0):
+            gr.Warning(f"No product found in dataBase, \nPlease try again later")
+        
+        return zip(link,name)
+    except Exception as e:
+        gr.Warning(f"Failed to fetch relevent products, \nPlease refresh and start again...")
+        print(e)
+
+
+text = gr.Text(label="Features")
+relevent_prod = gr.Gallery(columns=1,object_fit='contain',label="Relevent Products",height=600,rows=2)
+
+
+with gr.Blocks(title="Relevent Product Search",
+               css="footer {visibility: hidden}"    
+            ) as prodSearch:
+        gr.Interface(fn= Input_IMG_features,
+                        inputs=gr.Image(label='Upload Product Image',type='filepath',height="auto"),
+                        outputs= relevent_prod,
+                        title="<p style='color:orange; font-size:35px'>Search Relevent Products on One Click</p><br>",
+                        allow_flagging='never', 
+                        submit_btn="Search"
+                    )
+
+prodSearch.launch(debug=True)
