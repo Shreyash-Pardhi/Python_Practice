18d75016585103666db8813db3b7d04bb9ec40e9
unknown <shreyashpardhi1@gmail.com>
2024-07-02 18:55:15 +0530

    updates 2/7/2024

--- Python/Django Projects/E_Commerce/Store/views.py
+++ Python/Django Projects/E_Commerce/Store/views.py
@@ -40,35 +40,37 @@ def validateURL(urlDF):
 
 ###################### Saving Data to Cloud ######################
 def addProdToCloud(df:pd.DataFrame):
-    DB = pd.read_csv("gs://bucket-shreyash/Product_Data/Product_D.csv")
+    DB = pd.read_csv("gs://bucket-shreyash/Product_Data/ProductDEMO.csv")
     finalDF = pd.concat([DB, df], ignore_index=True)
     checkDuplicateData(finalDF)
-    finalDF.to_csv("gs://bucket-shreyash/Product_Data/Product_D.csv",index=False)
+    finalDF.to_csv("gs://bucket-shreyash/Product_Data/ProductDEMO.csv",index=False)
     
 ###################### Detecting Object from imageURL ######################
-def featureExtraction(uri):
-    txt = ''
-    client = vision.ImageAnnotatorClient()
-    img = vision.Image()
-    img.source.image_uri = uri
+# def featureExtraction(uri):
+#     txt = ''
+#     client = vision.ImageAnnotatorClient()
+#     img = vision.Image()
+#     img.source.image_uri = uri
     
-    res_label = client.label_detection(img)
-    labels = res_label.label_annotations
-    label = [lab.description for lab in labels]
+#     res_label = client.label_detection(img)
+#     labels = res_label.label_annotations
+#     label = [lab.description for lab in labels]
     
-    #for objects
-    objects = client.object_localization(image=img).localized_object_annotations
-    obj = [ob.name for ob in objects]
-    txt = txt + f"{label}" if len(obj)==0 else txt + f"{obj}"
-    return txt
+#     #for objects
+#     objects = client.object_localization(image=img).localized_object_annotations
+#     obj = [ob.name for ob in objects]
+#     txt = txt + f"{label}" if len(obj)==0 else txt + f"{obj}"
+#     return txt
 
 ###################### Precessing Data ######################
 def preProcessData(df:pd.DataFrame):
     dfURL = df['product_url']
     if(validateURL(dfURL)==True):
-        for i in range(len(dfURL)):
-            txt = featureExtraction(dfURL[i])
-            df.loc[i, "objects_extracted"] = txt
+        # for i in range(len(dfURL)):
+        #     txt = featureExtraction(dfURL[i])
+        #     df.loc[i, "objects_extracted"] = txt
+        # return df
+        checkDuplicateData(df)
         return df
     else:
         raise ValidationError("pre processing problem")
@@ -113,8 +115,8 @@ def addCSVfile(req):
             csvFile = req.FILES['csvFile']
             if csvFile:
                 df = validateCSVfile(csvFile)
-                data = preProcessData(df)
-                addProdToCloud(data)
+                # data = preProcessData(df)
+                addProdToCloud(df)
                 messages.success(req, 'Products in CSV added to Databese')
                 return redirect('adminHome')
         
@@ -129,7 +131,8 @@ def addCSVfile(req):
 ###################### Get all Products from bucket ######################
 @login_required(login_url='login')
 def getAllProd(req):
-    df = pd.read_csv("gs://bucket-shreyash/Product_Data/Product_D.csv")
+    df = pd.read_csv("gs://bucket-shreyash/Product_Data/ProductDEMO.csv")
+    df = df[::-1]
     data=[{"link":l, "name":n} for l,n in zip(df['product_url'],df['product_name'])]
     return render(req, 'UserHome.html', {'data':data}) 
 
@@ -167,9 +170,9 @@ def loginUSER(req):
             else:
                 return redirect('getAllProducts')
                 # return render(req, 'UserHome.html')
-
         else:
-            return messages.WARNING()
+            messages.error(req, 'Invalid Credentials, Please register if not registered')
+            return redirect('login')
     return render(req, 'Login.html')
 
 
@@ -182,4 +185,8 @@ def logoutUSER(req):
 ###################### Admin Page ######################
 @login_required(login_url='login')
 def adminUser(req):
-    return render(req, 'AdminHome.html')
+    if req.user.is_admin:
+        print(req.user.id)
+        return render(req, 'AdminHome.html')
+    else:
+        return HttpResponse('Page not found')
