d0b1a8ff0d694c28e0c1db213e50e0137149f5f9
unknown <shreyashpardhi1@gmail.com>
2024-07-25 18:56:52 +0530

    updates 25/7/2024

--- Django and Angular/E-commerce(Vision API)/frontend/src/app/Pages/register/register.component.ts
+++ Django and Angular/E-commerce(Vision API)/frontend/src/app/Pages/register/register.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { SharedAllService } from '../../shared-all.service';
 import { Router, RouterLink } from '@angular/router';
 import { LoaderService } from '../../loader.service';
@@ -7,7 +13,7 @@ import { LoaderService } from '../../loader.service';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
   providers: [SharedAllService, Router],
@@ -20,30 +26,44 @@ export class RegisterComponent {
   ) {}
 
   title: string = 'Register';
-  is_ad: boolean = false;
-  data: any = {
-    username: '',
-    email: '',
-    password1: '',
-    password2: '',
-    is_admin: this.is_ad,
-  };
+
+  pas = /^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{6,}$/;
+  Registration_data: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$'),
+    ]),
+    password1: new FormControl('', [
+      Validators.required,
+      Validators.pattern(this.pas),
+    ]),
+    password2: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+    is_admin: new FormControl(false),
+  });
+
+  // data: any = {
+  //   username: '',
+  //   email: '',
+  //   password1: '',
+  //   password2: '',
+  //   is_admin: false,
+  // };
   UserRegister() {
-    this.loaderService.showLoader();
-    this.service.registerUser(this.data).subscribe(
-      (res) => {
-        if (res.success) {
-          this.loaderService.hideLoader();
-          alert(res.message);
-          this.router.navigateByUrl('/login');
-        } else {
-          this.loaderService.hideLoader();
-          alert(res.message);
-        }
-      }
-      // (error) => {
-      //   console.error('Registration error:', error);
-      // }
-    );
+    // this.loaderService.showLoader();
+    // this.service.registerUser(this.data).subscribe((res) => {
+    //   if (res.success) {
+    //     this.loaderService.hideLoader();
+    //     alert(res.message);
+    //     this.router.navigateByUrl('/login');
+    //   } else {
+    //     this.loaderService.hideLoader();
+    //     alert(res.message);
+    //   }
+    // });
+    console.log(this.Registration_data.value);
   }
 }
